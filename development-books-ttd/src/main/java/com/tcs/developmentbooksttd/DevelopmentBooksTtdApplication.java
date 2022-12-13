6de351416468025a41d5f6f86dacc1e4afa39df5
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-13 12:52:00 +0530

    added spring boot project to the repo.

---
+++ development-books-ttd/src/main/java/com/tcs/developmentbooksttd/DevelopmentBooksTtdApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.developmentbooksttd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooksTtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBooksTtdApplication.class, args);
+	}
+
+}
